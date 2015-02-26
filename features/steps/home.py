eb02087a6cc2f0b21cfb6f3453dabcfab1f37c7f
Joe Friedl <joe@joefriedl.net>
2015-02-26 15:33:15 -0500

    Whoopsies

--- features/steps/home.py
+++ features/steps/home.py
@@ -5,6 +5,7 @@ from behave import when, then
 def step_impl(context):
     context.get_browser().get(context.base_url)
 
+
 @then(u'I should see "{text}" in the title')
 def step_impl(context, text):
     browser = context.get_browser()
