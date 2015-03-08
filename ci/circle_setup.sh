56f9ce3fe432b21acd70f0f431ed9626c4880449
Joe Friedl <joe@joefriedl.net>
2015-03-08 18:14:12 -0400

    Default to Python tests if not parallel

--- ci/circle_setup.sh
+++ ci/circle_setup.sh
@@ -3,16 +3,16 @@
 # Usage: circle_setup.sh
 
 case $CIRCLE_NODE_INDEX in
-    0)
+    1)
         ci/heroku_setup.sh \
             $CIRCLE_SHA1 \
             circle-minicomi-$CIRCLE_BUILD_NUM \
             $HEROKU_AUTH_TOKEN
         ;;
-    1)
+    2)
         ci/docker_setup.sh
         ;;
-    2)
+    *)
         ci/python_setup.sh
         ;;
 esac
--- ci/circle_test.sh
+++ ci/circle_test.sh
@@ -3,15 +3,15 @@
 # Usage: circle_test.sh
 
 case $CIRCLE_NODE_INDEX in
-    0)
+    1)
         ci/heroku_test.sh \
             circle-minicomi-$CIRCLE_BUILD_NUM \
             $HEROKU_AUTH_TOKEN
         ;;
-    1)
+    2)
         ci/docker_test.sh $DOCKER_IP
         ;;
-    2)
+    *)
         ci/python_test.sh
         ;;
 esac
