250d145dae5b2954769337ff1ca8df66813d1810
Joe Friedl <joe@joefriedl.net>
2015-03-08 19:39:12 -0400

    Don't do parallel setups?

--- ci/python_test.sh
+++ ci/python_test.sh
@@ -2,7 +2,29 @@
 #
 # Usage: python_test.sh
 
+export SUPERUSER_NAME=minicomi
+export SUPERUSER_PASSWORD=minicomi
+export SUPERUSER_EMAIL='minicomi+test@joefriedl.net'
+export DATABASE_URL='sqlite:///ci.db'
+export DJANGO_SETTINGS_MODULE='minicomi.settings.production'
+
 pep8 .
 coverage run --source='.' manage.py test
 
-coveralls
+# Set things up
+python manage.py migrate
+python manage.py minicomi_setup
+
+# Get that server going
+python manage.py runserver 127.0.0.1:5000 --noreload > server.log 2>&1 &
+MINICOMI_PID=$!
+
+behave
+
+# Get that server not going
+kill -TERM $MINICOMI_PID
+rm ci.db
+
+if [ -n "$COVERALLS_REPO_TOKEN" ]; then
+    coveralls
+fi
