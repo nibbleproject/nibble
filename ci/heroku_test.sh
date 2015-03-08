e9f0cbe416d94458ce3a6c849ab788cec9b87ba2
Joe Friedl <joe@joefriedl.net>
2015-03-08 17:31:29 -0400

    Maybe speed up those builds????

--- ci/heroku_test.sh
+++ ci/heroku_test.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 #
-# Usage: test-heroku.sh CURRENT_COMMIT HEROKU_AUTH_TOKEN
+# Usage: test-heroku.sh APP_NAME HEROKU_AUTH_TOKEN
 
 HEROKU_AUTH_TOKEN="$2"
-HEROKU_APP_NAME="ci-minicomi-`date '+%s'`"
+HEROKU_APP_NAME="$1"
 export MINICOMI_ALT_DOMAIN="$HEROKU_APP_NAME.joefriedl.net"
 export MINICOMI_BASE_URL="https://$HEROKU_APP_NAME.herokuapp.com"
 
 export SUPERUSER_NAME=minicomi
 export SUPERUSER_PASSWORD=minicomi
 export SUPERUSER_EMAIL='minicomi+test@joefriedl.net'
-
-# Create a Heroku app
-happy up \
-    --auth-token $HEROKU_AUTH_TOKEN \
-    --env SUPERUSER_NAME=$SUPERUSER_NAME \
-    --env SUPERUSER_PASSWORD=$SUPERUSER_PASSWORD \
-    --env SUPERUSER_EMAIL=$SUPERUSER_EMAIL \
-    --tarball-url https://github.com/grampajoe/minicomi/tarball/$1/ \
-    $HEROKU_APP_NAME
-
-# Add a custom domain
-curl -n -X POST https://api.heroku.com/apps/$HEROKU_APP_NAME/domains \
-    -H "Authorization: Bearer $HEROKU_AUTH_TOKEN" \
-    -H "Accept: application/vnd.heroku+json; version=3" \
-    -H "Content-Type: application/json" \
-    -d "{\"hostname\": \"$MINICOMI_ALT_DOMAIN\"}";
 
 # Run integration tests
 behave
