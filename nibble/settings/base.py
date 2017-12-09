5783392825fa0867d8299deeb573e659f800cc98
Joe Friedl <jfriedl@digitalocean.com>
2017-12-09 12:49:09 -0500

    Use the .get method

--- nibble/settings/base.py
+++ nibble/settings/base.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY', 'please-change-me')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'please-change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
