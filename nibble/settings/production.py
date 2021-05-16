d482ddeae0fdcc8dc6f248f59932ad76e17c1bde
Joe Friedl <joe@joefriedl.net>
2021-05-16 18:54:32 -0400

    Redirect http to https

--- nibble/settings/production.py
+++ nibble/settings/production.py
@@ -7,3 +7,7 @@ DEBUG = False
 DATABASES['default'] = dj_database_url.config()
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Redirect http:// to https://
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
